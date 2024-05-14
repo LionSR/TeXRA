3ff95c2715c6ca5272206971fbba2a681fc4bde6
Sirui Lu (MPQ office) <divergence_lu@icloud.com>
2024-05-14 20:24:59 +0200

    convert to word experiment

--- alias.sh
+++ alias.sh
@@ -66,3 +66,13 @@ function revised_supp_prl() {
     python "${SCRIPT_DIR}/reply_prl.py" --model=${MODEL} --prompt_path=${PROMPT_PATH} --example_reply_letter="${PROMPT_PATH}/example_reply_letter.txt" --task=revised_supp --input_file="${INPUT_FILE}" --main_content="${MAIN_CONTENT}" --supp_file="${SUPP_FILE}" --draft_reply_letter="${DRAFT_REPLY_LETTER}" --draft_main_content="${DRAFT_MAIN_CONTENT}" --cover_letter=rebuttal/cover_letter.txt --instruction=rebuttal/instruction.txt --editor_letter=rebuttal/editor_letter.txt --report_a=rebuttal/report_a.txt --report_b=rebuttal/report_b.txt
 }
 
+
+function polish_reply_prl() {
+    local INPUT_FILE=$1
+    local MAIN_CONTENT=$2
+    local SUPP_FILE=${3:-"supp.tex"}
+    local DRAFT_REPLY_LETTER=$1
+    local PROMPT_PATH=${SCRIPT_DIR}/prompts_reply
+    local MODEL=${MODEL:-opus}
+    python "${SCRIPT_DIR}/reply_prl.py" --model=${MODEL} --prompt_path=${PROMPT_PATH} --example_reply_letter="${PROMPT_PATH}/example_reply_letter.txt" --task=polish_reply --input_file="${INPUT_FILE}" --main_content="${MAIN_CONTENT}" --supp_file="${SUPP_FILE}" --draft_reply_letter="${DRAFT_REPLY_LETTER}" --cover_letter=rebuttal/cover_letter.txt --instruction=rebuttal/instruction.txt --editor_letter=rebuttal/editor_letter.txt --report_a=rebuttal/report_a.txt --report_b=rebuttal/report_b.txt
+}
