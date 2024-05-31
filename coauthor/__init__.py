fe85da0b996871c10cf17baccbfbe2a1460372c4
Sirui Lu (laptop) <divergence_lu@icloud.com>
2024-05-31 15:37:08 +0200

    file renames

--- coauthor/__init__.py
+++ coauthor/__init__.py
@@ -1,22 +1,33 @@
 # coauthor/__init__.py
 from .process import process_file_with_llm
-from .utils import (
+from .model_utils import (
+    create_response,
+    extract_response_statistics,
+    print_summary,
+    is_openai_model,
+    is_anthropic_model,
+    compute_api_price,
+)
+
+from .file_utils import (
     read_file,
     write_file,
     find_last_non_empty_line,
     extract_text_from_tags,
     check_for_massive_repetition,
-    compute_api_price,
-    model_mapping,
 )
 
 __all__ = [
     "process_file_with_llm",
+    "create_response",
+    "extract_response_statistics",
+    "print_summary",
+    "is_openai_model",
+    "is_anthropic_model",
     "read_file",
     "write_file",
     "find_last_non_empty_line",
     "extract_text_from_tags",
     "check_for_massive_repetition",
     "compute_api_price",
-    "model_mapping",
 ]
