b076d39fe41038bb11ac4bcf819fe8d70f18f32d
Sirui Lu (MPQ office) <divergence_lu@icloud.com>
2024-06-03 21:38:21 +0200

    improve cli commands

--- coauthor/cli.py
+++ coauthor/cli.py
@@ -2,6 +2,7 @@ import click
 import subprocess
 import os
 import glob
+import fnmatch
 
 
 @click.group()
@@ -29,6 +30,7 @@ def correct_tex(input_file, auxiliary_file=None):
 
 @click.command()
 def clean():
+    excluded_dirs = {'Figs', 'Figures', 'build'}
     patterns = [
         "*_opus.tex",
         "*_sonnet.tex",
@@ -37,37 +39,29 @@ def clean():
         "*_gpt4o.tex",
     ]
     patterns_build = [
-        "build/*_opus*",
-        "build/*_sonnet*",
-        "build/*_haiku*",
-        "build/*_gpt4t*",
-        "build/*_gpt4o*",
+        "*/build/*_opus*",
+        "*/build/*_sonnet*",
+        "*/build/*_haiku*", 
+        "*/build/*_gpt4t*",
+        "*/build/*_gpt4o*",
     ]
     files_to_delete = []
-    for pattern in patterns:
-        # Search in the current directory and immediate subdirectories
-        files_to_delete.extend(glob.glob(pattern))
-    for pattern in patterns_build:
-        files_to_delete.extend(glob.glob(pattern))
 
+    # Walk through all directories and files, excluding specific directories
+    for root, dirs, files in os.walk(".", topdown=True):
+        dirs[:] = [d for d in dirs if d not in excluded_dirs]  # Modify dirs in-place to exclude certain directories
+        for pattern in patterns:
+            for filename in fnmatch.filter(files, pattern):
+                files_to_delete.append(os.path.join(root, filename))
+        
+        for pattern in patterns_build:
+            files_to_delete.extend(glob.glob(os.path.join(root, pattern), recursive=True))
+
+    # Perform the deletion
     for file in files_to_delete:
         os.remove(file)
 
     print("Cleanup complete.")
-
-
-@click.command()
-def indent_tex():
-    tex_files = glob.glob(
-        "./*.tex", recursive=False
-    )  # Find all .tex files in the current directory
-    for tex_file in tex_files:
-        subprocess.run(
-            ["latexindent", tex_file, "-w", "-s", "-l"],
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL,
-        )
-    print("All .tex files have been indented.")
 
 
 @click.command()
@@ -84,13 +78,40 @@ def rm_build():
     for build_file in build_files:
         os.remove(build_file)
 
+    # Delete all PDF files and files in the build directory in subdirectories, excluding 'Figs' and 'Figures'
+    for root, dirs, files in os.walk(".", topdown=True):
+        dirs[:] = [d for d in dirs if d not in {'Figs', 'Figures'}]  # Exclude 'Figs' and 'Figures' directories
+        for file in files:
+            if file.endswith('.pdf') or os.path.basename(os.path.dirname(file)) == 'build':
+                os.remove(os.path.join(root, file))
+
     print("All specified files have been deleted.")
+
+
+@click.command()
+def indent_tex():
+    tex_files = glob.glob(
+        "./*.tex", recursive=False
+    )  # Find all .tex files in the current directory
+    for tex_file in tex_files:
+        subprocess.run(
+            ["latexindent", tex_file, "-w", "-s", "-l"],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+    
+    # Delete all .bak and indent.log files in the current directory
+    files_to_delete = glob.glob("./*.bak0", recursive=False) + glob.glob("./indent.log", recursive=False)
+    for file in files_to_delete:
+        os.remove(file)
+
+    print("All .tex files have been indented and .bak files have been deleted.")
 
 
 cli.add_command(correct_tex)
 cli.add_command(clean)
-cli.add_command(indent_tex)
 cli.add_command(rm_build)
+cli.add_command(indent_tex)
 
 
 if __name__ == "__main__":
