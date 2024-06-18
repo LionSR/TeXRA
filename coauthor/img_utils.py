e6c21c931d68a43bb1468964dad822c7b9703e0b
Sirui Lu <14889516+LionSR@users.noreply.github.com>
2024-06-18 15:03:19 +0200

    multimodal notebooks

---
+++ coauthor/img_utils.py
@@ -0,0 +1,10 @@
+import base64
+from IPython.display import Image
+
+
+def get_base64_encoded_image(image_path):
+    with open(image_path, "rb") as image_file:
+        binary_data = image_file.read()
+        base_64_encoded_data = base64.b64encode(binary_data)
+        base64_string = base_64_encoded_data.decode("utf-8")
+        return base64_string
