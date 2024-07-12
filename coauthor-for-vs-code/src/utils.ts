8853650bdbceda6ad23db5e1b396478be7366330
Sirui Lu <14889516+LionSR@users.noreply.github.com>
2024-07-12 14:13:05 +0200

    feat: Add support for sample files and improve UI
    - Implement sample file selection in UI
    - Add backend support for sample files
    - Update task options and descriptions
    - Improve file handling and diff operations
    - Minor UI enhancements and bug fixes

--- coauthor-for-vs-code/src/utils.ts
+++ coauthor-for-vs-code/src/utils.ts
@@ -10,6 +10,15 @@ export async function listInputFiles(): Promise<string[]> {
   return [];
 }
 
+export async function listSampleFiles(): Promise<string[]> {
+  const workspaceFolders = vscode.workspace.workspaceFolders;
+  if (workspaceFolders) {
+    const workspacePath = workspaceFolders[0].uri.fsPath;
+    return await getFilesRecursively(workspacePath, workspacePath, ['.txt', '.tex'], ['.pdf', '.bst', '.bib', '.cls', '.sty', '.json', '.py', '.ipynb', '.png', '.pdf', '.vslx', '.ts', '.js'], ['build', 'node_modules', 'figures', 'Figs', '__pycache__', 'Figures', 'figs', 'Versions'], ['_log_', 'Makefile', 'template', '_log', '_diff', 'command.tex', 'preamble.tex', 'diff', 'draw']);
+  }
+  return [];
+}
+
 export async function listAuxFiles(): Promise<string[]> {
   const workspaceFolders = vscode.workspace.workspaceFolders;
   if (workspaceFolders) {
