3af6a89f68fb074eb9ff780e13a7f6ae3a7e50b9
Sirui Lu <14889516+LionSR@users.noreply.github.com>
2024-07-18 15:31:44 +0200

    Bump version to 0.6.7 and refactor getCurrentEditedFile command
    - Rename 'getCurrentEditedFile' command to 'requestEditedFile' in script.js
    - Remove unused 'getCurrentEditedFile' case in viewProvider.ts
    - Minor code formatting improvements in terminal.ts

--- coauthor-for-vs-code/src/terminal.ts
+++ coauthor-for-vs-code/src/terminal.ts
@@ -3,11 +3,11 @@ import * as vscode from 'vscode';
 let terminal: vscode.Terminal | undefined;
 
 export function ensureTerminal() {
-    const existingTerminal = vscode.window.terminals.find(t => t.name === 'housekeeping');
-    if (existingTerminal) {
-        terminal = existingTerminal;
-    } else if (!terminal || terminal.exitStatus !== undefined) {
-        terminal = vscode.window.createTerminal('housekeeping');
-    }
-    return terminal;
+  const existingTerminal = vscode.window.terminals.find(t => t.name === 'housekeeping');
+  if (existingTerminal) {
+    terminal = existingTerminal;
+  } else if (!terminal || terminal.exitStatus !== undefined) {
+    terminal = vscode.window.createTerminal('housekeeping');
+  }
+  return terminal;
 }
