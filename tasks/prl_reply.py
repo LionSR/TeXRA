83cb5138c89cbfa8d8c09cd51ba1087060b78e1d
Sirui@mba <divergence_lu@icloud.com>
2024-08-01 23:01:36 +0200

    style: format code with black

--- tasks/prl_reply.py
+++ tasks/prl_reply.py
@@ -7,17 +7,19 @@ prompt_path = coa.get_prompt_path(coa, "prl_reply")
 class PRLReply(DirectWrite):
     def get_user_vars(self):
         user_vars = coa.get_user_vars(self.args)
-        user_vars.update({
-            "PREAMBLE_CONTENT": coa.read_file(self.args.preamble),
-            "MAIN_CONTENT": coa.read_file(self.args.input_file),
-            "SUPP_CONTENT": coa.read_file(self.args.supp_file) if self.args.supp_file else "",
-            "INSTRUCTION": coa.read_file(self.args.instruction) if self.args.instruction else "",
-            "COVER_LETTER": coa.read_file(self.args.cover_letter) if self.args.cover_letter else "",
-            "EDITOR_DECISION_LETTER": coa.read_file(self.args.editor_letter) if self.args.editor_letter else "",
-            "REFEREE_REPORT_A": coa.read_file(self.args.report_a) if self.args.report_a else "",
-            "REFEREE_REPORT_B": coa.read_file(self.args.report_b) if self.args.report_b else "",
-            "EXAMPLE_REPLY_LETTER": coa.read_file(self.args.example_reply_letter) if self.args.example_reply_letter else "",
-        })
+        user_vars.update(
+            {
+                "PREAMBLE_CONTENT": coa.read_file(self.args.preamble),
+                "MAIN_CONTENT": coa.read_file(self.args.input_file),
+                "SUPP_CONTENT": coa.read_file(self.args.supp_file) if self.args.supp_file else "",
+                "INSTRUCTION": coa.read_file(self.args.instruction) if self.args.instruction else "",
+                "COVER_LETTER": coa.read_file(self.args.cover_letter) if self.args.cover_letter else "",
+                "EDITOR_DECISION_LETTER": coa.read_file(self.args.editor_letter) if self.args.editor_letter else "",
+                "REFEREE_REPORT_A": coa.read_file(self.args.report_a) if self.args.report_a else "",
+                "REFEREE_REPORT_B": coa.read_file(self.args.report_b) if self.args.report_b else "",
+                "EXAMPLE_REPLY_LETTER": coa.read_file(self.args.example_reply_letter) if self.args.example_reply_letter else "",
+            }
+        )
 
         if "revise" in self.args.task or "polish" in self.args.task:
             user_vars["DRAFT_REPLY_LETTER"] = coa.read_file(self.args.draft_reply_letter) if self.args.draft_reply_letter else ""
@@ -39,8 +41,7 @@ def main():
     parser.add_argument("--supp_file", type=str, help="Path to the supplementary TeX file to be included in the response.", default=None)
     parser.add_argument("--instruction", type=str, help="Path to the file containing the overall instruction.")
     parser.add_argument(
-        "--task", type=str, default="reply_letter", help="Mode of operation.", 
-        choices=["reply_letter", "revise_main", "revise_supp", "polish_reply"]
+        "--task", type=str, default="reply_letter", help="Mode of operation.", choices=["reply_letter", "revise_main", "revise_supp", "polish_reply"]
     )
     parser.add_argument("--cover_letter", type=str, help="Path to the cover letter file.")
     parser.add_argument("--editor_letter", type=str, help="Path to the editor letter file.")
