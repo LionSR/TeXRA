034d8b1768ce41edf0958f592040e32076cc1121
Sirui@MPQ <divergence_lu@icloud.com>
2024-08-09 18:05:44 +0200

    try to make the fix for PRL reply

--- tasks/prl_edit.py
+++ tasks/prl_edit.py
@@ -1,17 +1,17 @@
-from coauthor.base_tasks import DirectWrite
+from coauthor.base_tasks import ThinkWrite
 import coauthor as coa
 
 prompt_path = coa.get_prompt_path(coa, "prl")
 
 
-class PRLEdit(DirectWrite):
+class PRLEdit(ThinkWrite):
     def get_user_vars(self):
         user_vars = coa.get_user_vars_basic(self.args)
         user_vars.update(
             {
                 "PREAMBLE_CONTENT": coa.read_file(self.args.preamble),
-                "MAIN_FILE": self.args.main_file,
-                "MAIN_CONTENT": coa.read_file(self.args.main_file),
+                "MAIN_FILE": self.args.input_file,
+                "MAIN_CONTENT": coa.read_file(self.args.input_file),
                 "SUPP_FILE": self.args.supp_file,
                 "SUPP_CONTENT": coa.read_file(self.args.supp_file),
             }
@@ -22,10 +22,20 @@ class PRLEdit(DirectWrite):
 def main():
     parser = coa.get_common_argparser()
     parser.add_argument("--preamble", default="preamble.tex", type=str, help="Path to the preamble TeX file.")
-    parser.add_argument("--main_file", type=str, required=True, help="Path to the main TeX file to be processed.")
-    parser.add_argument("--supp_file", type=str, required=True, help="Path to the supplementary TeX file to be processed.")
+    parser.add_argument("--main_file", type=str, required=False, help="Path to the main TeX file to be processed.")
+    parser.add_argument("--supp_file", type=str, required=False, help="Path to the supplementary TeX file to be processed.")
     parser.add_argument("--task", type=str, default="correct_prl", help="Mode of operation, 'correct_prl' for both main and supplementary materials.")
     args = parser.parse_args()
+
+    # within args.input_files find the one that has supp and set it as supp_file is supp file is not given
+    if not args.supp_file:
+        for f in args.input_files:
+            if "supp" in f:
+                args.supp_file = f
+                break
+
+    if not args.output_files:
+        args.output_files = [args.input_file, args.supp_file]
 
     prl_edit = PRLEdit(args, prompt_path)
     prl_edit.run()
