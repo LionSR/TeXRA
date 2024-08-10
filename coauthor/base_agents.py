e56a1f50243af9c2fa52b13c184e431380d2a603
Sirui Lu <14889516+LionSR@users.noreply.github.com>
2024-08-10 16:28:57 +0200

    refactor: Simplify get_output_file method call in ThinkAndWrite class

--- coauthor/base_agents.py
+++ coauthor/base_agents.py
@@ -191,7 +191,7 @@ class ThinkAndWrite(BaseReflectChainAgent):
     def get_output_file(self, round=0):
         base_output_file = self.args.output_name_override if self.args.output_name_override else self.args.input_file
         file_extension = "xml"
-        return get_output_file_name(base_output_file, self.args.agent, self.model_settings["model"], file_extension, round=round)
+        return get_output_file_name(base_output_file, self.args.agent, self.model_settings["model"], file_extension, round)
 
     def handle_output(self, state, end_turn, output_file, is_reflection_complete=False):
         if end_turn:
