5daf43994df1eca45a181807be30a560a18f1f86
Sirui@MPQ <divergence_lu@icloud.com>
2024-08-10 18:23:06 +0200

    fix prl prompts

--- agents/edit_prl.py
+++ agents/edit_prl.py
@@ -1,10 +1,10 @@
-from coauthor.base_agents import ThinkAndWrite
+from coauthor.base_agents import ThinkAndWrite, DirectWrite
 import coauthor as coa
 
 agent_path = coa.get_agent_path(coa, "prl")
 
 
-class EditPRL(ThinkAndWrite):
+class EditPRLBase:
     def get_user_vars(self):
         user_vars = coa.get_user_vars_basic(self.args)
         user_vars.update(
@@ -20,15 +20,24 @@ class EditPRL(ThinkAndWrite):
         return user_vars
 
 
+class EditPRLThink(EditPRLBase, ThinkAndWrite):
+    pass
+
+
+class EditPRLDirect(EditPRLBase, DirectWrite):
+    pass
+
+
 def main():
     parser = coa.get_common_argparser()
     parser.add_argument("--preamble_file", default="preamble.tex", type=str, help="Path to the preamble TeX file.")
     parser.add_argument("--main_file", type=str, required=False, help="Path to the main TeX file to be processed.")
     parser.add_argument("--supp_file", type=str, required=False, help="Path to the supplementary TeX file to be processed.")
-    parser.add_argument("--agent", type=str, default="correct_prl", help="Mode of operation, 'correct_prl' for both main and supplementary materials.")
+    parser.add_argument(
+        "--agent", type=str, default="correct_prl", choices=["correct_prl", "polish_prl"], help="Mode of operation, 'correct_prl' or 'polish_prl'."
+    )
     args = parser.parse_args()
 
-    # within args.input_files find the one that has supp and set it as supp_file is supp file is not given
     if not args.supp_file:
         for f in args.input_files:
             if "supp" in f:
@@ -38,8 +47,14 @@ def main():
     if not args.output_files:
         args.output_files = [args.input_file, args.supp_file]
 
-    edit_prl = EditPRL(args, agent_path)
+    if args.agent.startswith("correct"):
+        edit_prl = EditPRLDirect(args, agent_path)
+    else:
+        edit_prl = EditPRLThink(args, agent_path)
     edit_prl.run()
+
+    print(f"First round output files: {edit_prl.first_round_output_files}")
+    print(f"Reflect round output files: {edit_prl.reflect_round_output_files}")
 
 
 if __name__ == "__main__":
