c291e11319168e71673b353d49d87d45e74f2e96
Sirui Lu <14889516+LionSR@users.noreply.github.com>
2024-10-12 22:36:27 +0200

    add chatgpt-4o-latest/improve translation prompts

--- coauthor/output_utils.py
+++ coauthor/output_utils.py
@@ -49,6 +49,7 @@ def ensure_correct_xml_structure(file_path, document_tag):
                 "\\end{document}\n<document name": "\\end{document}\n</document>\n<document name",
                 "\\end{document}\n</latex_documents>": "\\end{document}\n</document>\n</latex_documents>",
                 "<scratchpad><scratchpad>": "<scratchpad>",
+                "<scratchpad>\n<scratchpad>": "<scratchpad>",
                 "<scratchpad><latex_document>": "<scratchpad></scratchpad><latex_document>",
                 "\end{scratchpad>": "</scratchpad>",
             }
