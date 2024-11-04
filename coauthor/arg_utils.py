a68ca1509eb28a6f1c0ea5571c433d0b4ce1b54b
Sirui@mba <divergence_lu@icloud.com>
2024-11-04 20:52:57 +0100

    add haiku 3.5

--- coauthor/arg_utils.py
+++ coauthor/arg_utils.py
@@ -18,6 +18,7 @@ def get_common_argparser():
             "sonnet+",
             "opus",
             "sonnet",
+            "haiku++",
             "haiku",
             "gpto1",
             "gpto1-",
