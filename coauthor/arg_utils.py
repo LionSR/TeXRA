4342955cce904abdda9d395b57f3cf3e461c48a4
Sirui@MPQ <divergence_lu@icloud.com>
2024-11-15 18:39:39 +0100

    suppport gemini models via openai compatiable apis

--- coauthor/arg_utils.py
+++ coauthor/arg_utils.py
@@ -1,4 +1,5 @@
 import argparse
+from model_utils import model_mapping
 
 
 def comma_separated_list(value):
@@ -13,24 +14,7 @@ def get_common_argparser():
         type=str,
         default="sonnet+",
         help="Model name to use for processing.",
-        choices=[
-            "sonnet++",
-            "sonnet+",
-            "opus",
-            "sonnet",
-            "haiku+",
-            "haiku",
-            "gpto1",
-            "gpto1-",
-            "gpt4t",
-            "gpt4o",
-            "gpt4o-",
-            "gpt4ol",
-            "gpt4oOR",
-            "gemini1p+OR",
-            "gemini1f+OR",
-            "llama3+OR",
-        ],
+        choices=list(model_mapping.keys()),
     )
     parser.add_argument(
         "--temperature",
