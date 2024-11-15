279b01ff9cc1e9589ed35074dfb37745430a9361
Sirui Lu <14889516+LionSR@users.noreply.github.com>
2024-11-15 16:36:51 +0100

    add google model via openai api

--- coauthor/model_utils.py
+++ coauthor/model_utils.py
@@ -7,7 +7,7 @@ model_mapping = {
     "sonnet+": "claude-3-5-sonnet-20240620",
     "opus": "claude-3-opus-20240229",
     "sonnet": "claude-3-sonnet-20240229",
-    "haiku++": "claude-3-5-haiku-20241022",
+    "haiku+": "claude-3-5-haiku-20241022",
     "haiku": "claude-3-haiku-20240307",
     "gpto1": "o1-preview-2024-09-12",
     "gpto1-": "o1-mini-2024-09-12",
@@ -16,6 +16,8 @@ model_mapping = {
     "gpt4o-": "gpt-4o-mini-2024-07-18",
     "gpt4ol": "chatgpt-4o-latest",
     "gpt4oOR": "openai/gpt-4o:extended",
+    "gemini1p+": "gemini-pro-1.5-latest",
+    "gemini1f+": "gemini-flash-1.5-latest",
     "gemini1p+OR": "google/gemini-pro-1.5",
     "gemini1f+OR": "google/gemini-flash-1.5",
     "llama3+OR": "meta-llama/llama-3.1-405b-instruct",
@@ -38,7 +40,7 @@ anthropic_models = {
     "opus",
     "sonnet",
     "haiku",
-    "haiku++",
+    "haiku+",
 }
 
 openai_models = {
@@ -48,6 +50,11 @@ openai_models = {
     "gpt4ol",
     "gpto1",
     "gpto1-",
+}
+
+google_models = {
+    "gemini1p+",
+    "gemini1f+",
 }
 
 openrouter_models = {
@@ -68,11 +75,25 @@ def is_openai_model(model):
 
 
 def is_anthropic_model(model):
-    return model in anthropic_models
+    return any(name in model for name in ("opus", "haiku", "sonnet"))
+
+
+def is_google_model(model):
+    return "gemini" in model
 
 
 def is_openrouter_model(model):
     return "OR" in model or "/" in model
+
+
+def is_openai_compatible_model(model):
+    if "gpt" in model:
+        return True
+    if model in openai_models:
+        return True
+    elif is_openrouter_model(model) and model.split("/")[1] in openai_models:
+        return True
+    return False
 
 
 def get_model_client(model):
@@ -81,7 +102,7 @@ def get_model_client(model):
     if is_openrouter_model(model):
         from openai import OpenAI
 
-        return OpenAI(base_url="https://openrouter.ai/api/v1", api_key=os.getenv("OPENROUTER_API_KEY"))
+        return OpenAI(api_key=os.getenv("OPENROUTER_API_KEY"), base_url="https://openrouter.ai/api/v1")
     elif is_openai_model(model):
         from openai import OpenAI
 
@@ -90,7 +111,8 @@ def get_model_client(model):
         from anthropic import Anthropic
 
         return Anthropic(api_key=os.getenv("ANTHROPIC_API_KEY"))
-
+    elif is_google_model(model):
+        return OpenAI(api_key=os.getenv("GOOGLE_API_KEY"), base_url="https://generativelanguage.googleapis.com/v1beta")
     else:
         raise ValueError("Unsupported model type")
 
@@ -100,19 +122,25 @@ def get_model_settings(args):
     model_name = model_mapping[model]
 
     max_tokens_mapping = {
-        "openai/gpt-4o:extended": 64000,
-        # "google/gemini-pro-1.5-exp": 32768,
-        "google/gemini-pro-1.5": 32768,
-        "google/gemini-flash-1.5": 32768,
-        "meta-llama/llama-3-1b-8192": 131072,
+        # anthropic models
         "claude-3-5-sonnet-20240620": 8192,
         "claude-3-5-sonnet-20241022": 8192,
         "claude-3-5-haiku-20241022": 8192,
+        "claude-3-opus-20240229": 4096,
+        # openai models
         "gpt-4o-mini-2024-07-18": 16384,
         "gpt-4o-2024-08-06": 16384,
+        "chatgpt-4o-latest": 16384,
         "o1-preview-2024-09-12": 32768,
         "o1-mini-2024-09-12": 65536,
-        "chatgpt-4o-latest": 16384,
+        # google models
+        "gemini-pro-1.5-latest": 32768,
+        "gemini-flash-1.5-latest": 32768,
+        # openrouter models
+        "openai/gpt-4o:extended": 64000,
+        "google/gemini-pro-1.5": 32768,
+        "google/gemini-flash-1.5": 32768,
+        "meta-llama/llama-3-1b-8192": 131072,
     }
 
     model_settings = {
@@ -132,7 +160,7 @@ def compute_api_price(model, input_tokens, output_tokens, cache_creation_input_t
         "sonnet++": (3, 15),
         "opus": (15, 75),
         "haiku": (0.25, 1.25),
-        "haiku++": (1, 5.0),
+        "haiku+": (1, 5.0),
         "gpt4t": (10, 30),
         "gpt4o": (2.5, 10),
         "gpt4o-": (0.15, 0.6),
@@ -140,11 +168,13 @@ def compute_api_price(model, input_tokens, output_tokens, cache_creation_input_t
         "gpto1": (15, 60),
         "gpto1-": (3, 12),
         "gpt4ol": (5, 15),
+        "gemini1p+": (2.5, 7.5),
+        "gemini1f+": (0.075, 0.3),
         "gemini1p+OR": (2.5, 7.5),
         "gemini1f+OR": (0.075, 0.3),
         "llama3+OR": (3, 3),
     }
-    models_with_prompt_caching_support = ["sonnet++", "sonnet+", "haiku", "opus", "haiku++"]
+    models_with_prompt_caching_support = ["sonnet++", "sonnet+", "haiku", "opus", "haiku+"]
 
     prompt_cache_creation_prices = {
         model: tuple(rate * 1.25 for rate in rates) for model, rates in prices.items() if model in models_with_prompt_caching_support
