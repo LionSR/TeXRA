2bf4f2abdbbd92a2a26039ec4406ef7e73b31890
Sirui Lu <14889516+LionSR@users.noreply.github.com>
2024-11-16 00:03:21 +0100

    consistency for filenames

--- agents/grant.py
+++ agents/grant.py
@@ -9,12 +9,12 @@ class ReviseGrant(ThinkAndWrite):
         user_vars = coa.get_user_vars_basic(self.args)
 
         if self.args.input_files:
-            user_vars["ADDITIONAL_INPUT_CONTENTS"] = coa.get_xml_format_from_files(self.args.input_files)
+            user_vars["ADDITIONAL_INPUTS"] = coa.get_xml_format_from_files(self.args.input_files)
 
         if self.args.auxiliary_files:
-            user_vars["AUXILIARY_FILE_CONTENTS"] = coa.get_xml_format_from_files(self.args.auxiliary_files)
+            user_vars["AUXILIARY_FILES"] = coa.get_xml_format_from_files(self.args.auxiliary_files)
         else:
-            user_vars["AUXILIARY_FILE_CONTENTS"] = ""
+            user_vars["AUXILIARY_FILES"] = ""
 
         if self.args.output_files:
             user_vars["OUTPUT_FILES_ORDER"] = ", ".join(self.args.output_files)
