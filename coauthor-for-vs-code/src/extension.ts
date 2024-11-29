8ae4c0469f99a11eea53e09d9e5d7f9b12de0374
Sirui Lu <14889516+LionSR@users.noreply.github.com>
2024-11-29 23:46:36 +0100

    feat: add file explorer view for CoAuthor agents

--- coauthor-for-vs-code/src/extension.ts
+++ coauthor-for-vs-code/src/extension.ts
@@ -1,8 +1,17 @@
 import * as vscode from 'vscode';
 import { registerCommands } from './commands';
+import { FolderExplorer } from './folderExplorer';
 
 export function activate(context: vscode.ExtensionContext) {
   registerCommands(context);
+
+  // Register the folder explorer
+  const workspaceRoot = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
+  const folderExplorer = new FolderExplorer(workspaceRoot);
+  
+  context.subscriptions.push(
+    vscode.window.registerTreeDataProvider('coauthor.folderExplorer', folderExplorer)
+  );
 }
 
 export function deactivate() {}
