f94f228cbe718858b877f4216cf9d8fd80158f5a
Sirui@MPQ <divergence_lu@icloud.com>
2024-12-04 17:15:05 +0100

    refactor: simplify merge agent by moving logic to AgentMerge class

--- coauthor/__init__.py
+++ coauthor/__init__.py
@@ -1,32 +1,31 @@
+from .agent_merge import AgentMerge
 from .agent_reflect import ThinkAndWrite, DirectWrite
-from .agent_reflect import get_output_file_name_merge
-from .arg_utils import get_common_argparser, comma_separated_list
-from .figure_tools import extract_figure_paths, extract_and_compile_tikzpictures_with_labels
+from .arg_utils import comma_separated_list, get_common_argparser
+from .config import AgentConfig, AgentPrompts, AgentSettings
+from .figure_tools import extract_and_compile_tikzpictures_with_labels, extract_figure_paths
 from .file_utils import (
+    append_file,
+    extract_text_from_tags,
+    get_agent_dir_from_env,
+    get_agent_path,
     read_file,
     write_file,
-    append_file,
     write_to_output_file,
-    extract_text_from_tags,
-    get_agent_path,
-    get_agent_dir_from_env,
-)
-from .tex_tools import run_latexdiff, run_latexdiff_vc, get_tex_count
-from .output_utils import (
-    check_for_massive_repetition,
-    ensure_correct_xml_structure,
-    split_scratchpad_output_xml,
-    split_multiple_scratchpad_output_xml,
-    add_cdata_to_tags,
-    add_cdata_to_tags_multiple,
 )
 from .img_utils import get_base64_encoded_image, single_page_pdf_to_png
-from .logdb_utils import logdb_start, logdb_and_print_statistics, logdb_output_files
+from .logdb_utils import logdb_and_print_statistics, logdb_output_files, logdb_start
 from .openai_utils import best_connection_method
-from .prompt_utils import (
-    load_agent_settings_and_prompts,
-    get_xml_format_from_files,
+from .output_utils import (
+    add_cdata_to_tags,
+    add_cdata_to_tags_multiple,
+    check_for_massive_repetition,
+    ensure_correct_xml_structure,
+    split_multiple_scratchpad_output_xml,
+    split_scratchpad_output_xml,
 )
-
+from .prompt_utils import (
+    get_xml_format_from_files,
+    load_agent_settings_and_prompts,
+)
 from .state import State
-from .config import AgentPrompts, AgentSettings, AgentConfig
+from .tex_tools import get_tex_count, run_latexdiff, run_latexdiff_vc
