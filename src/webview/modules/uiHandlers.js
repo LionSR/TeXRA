138b248e22bb707356274290bb6a99cd6879e404
Sirui@mba <divergence_lu@icloud.com>
2025-02-19 23:12:48 +0100

    fix: preserve file selections during file list refresh operations
    - Maintain selected file when base file list is updated
    - Preserve edited file selection when requesting updated edited file list
    - Add state management to handle file selection persistence

--- src/webview/modules/uiHandlers.js
+++ src/webview/modules/uiHandlers.js
@@ -282,22 +282,6 @@ export function setupUIHandlers() {
     });
   });
 
-  // Add event listener for the refresh button
-  // addEventListenerSafely('refreshEditedFileButton', 'click', function () {
-  //   const baseFile = safeGetElementValue('baseFile');
-  //   if (baseFile) {
-  //     vscode.postMessage({
-  //       command: 'requestEditedFile',
-  //       baseFile: baseFile,
-  //     });
-  //   } else {
-  //     vscode.postMessage({
-  //       command: 'showInformationMessage',
-  //       text: 'Please select a base file first.',
-  //     });
-  //   }
-  // });
-
   ['pack', 'clean'].forEach((action) => {
     addEventListenerSafely(`${action}Button`, 'click', function () {
       const inputFile = safeGetElementValue('inputFile');
