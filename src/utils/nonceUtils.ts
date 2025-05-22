ada7a92d9f618e1f95f5bfd9a9226d13767add92
Ray <divergence_lu@icloud.com>
2025-05-22 14:30:25 +0200

    refactor: centralize nonce generation

---
+++ src/utils/nonceUtils.ts
@@ -0,0 +1,10 @@
+// Utility to generate nonce strings for Webview content security policy
+export function generateNonce(length = 32): string {
+  const possible =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let text = '';
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
