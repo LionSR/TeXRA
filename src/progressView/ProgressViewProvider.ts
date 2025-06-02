94fadc78d354bb6dacfc25bbe8b4c66476f35dae
Sirui Lu <14889516+LionSR@users.noreply.github.com>
2025-06-02 18:59:53 +0200

    feat: load shared modules from common directory

--- src/progressView/ProgressViewProvider.ts
+++ src/progressView/ProgressViewProvider.ts
@@ -282,6 +282,7 @@ export class ProgressViewProvider implements vscode.WebviewViewProvider {
       localResourceRoots: [
         vscode.Uri.joinPath(this._extensionUri, 'src', 'progressView'),
         vscode.Uri.joinPath(this._extensionUri, 'src', 'common', 'styles'),
+        vscode.Uri.joinPath(this._extensionUri, 'src', 'common', 'modules'),
         vscode.Uri.joinPath(this._extensionUri, 'node_modules', 'split.js'),
         vscode.Uri.joinPath(
           this._extensionUri,
