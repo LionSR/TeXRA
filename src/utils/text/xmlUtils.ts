e13a2a1e7ad9dabdc669c46052ac4279acae0e93
Sirui Lu <14889516+LionSR@users.noreply.github.com>
2025-06-14 20:52:23 +0200

    feat: add root path aliases

--- src/utils/text/xmlUtils.ts
+++ src/utils/text/xmlUtils.ts
@@ -1,7 +1,7 @@
 // Local imports - log
-import * as logger from '../../logger/logUtils';
-import { AgentLogger } from '../../logger/AgentLogger';
-import { K_SLICE } from '../config';
+import * as logger from '@/logger/logUtils';
+import { AgentLogger } from '@/logger/AgentLogger';
+import { K_SLICE } from '@/utils/config';
 
 const CHANNEL = 'xmlUtils';
 logger.initialize(CHANNEL);
