044078c0b8876dbacef06016d0f297cc8499c505
Ray <divergence_lu@icloud.com>
2025-06-14 20:52:23 +0200

    feat: add root path aliases

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,10 @@ const extensionConfig = {
   resolve: {
     // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
     extensions: ['.ts', '.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '~': path.resolve(__dirname, 'src'),
+    },
     fallback: {
       fs: false,
       path: require.resolve('path-browserify'),
