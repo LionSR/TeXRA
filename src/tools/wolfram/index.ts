c55bb9d6ae58c9f98043e7e426a02a328f225a6a
Sirui Lu <14889516+LionSR@users.noreply.github.com>
2025-06-21 23:14:38 +0200

    chore: remove wolfram alpha utilities

--- src/tools/wolfram/index.ts
+++ src/tools/wolfram/index.ts
@@ -1,6 +1,3 @@
-// Export WolframAlpha client
-export { default as WolframAlphaClient } from './wolframAlphaUtils';
-
 // Export wolframscript utilities
 export { default as WolframScript } from './wolframScriptUtils';
 export * from './wolframScriptUtils';
