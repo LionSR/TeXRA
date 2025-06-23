6a976fd78b7c3070498b3c5391efc08927f436a6
Sirui@mba <divergence_lu@icloud.com>
2025-06-24 00:52:59 +0200

    style: fix whitespace and cleanup logging - Remove extra whitespace in YAML files - Change log level from info to debug for end tag detection

--- src/agent/modelHandlers/modelHandlerOpenAI.ts
+++ src/agent/modelHandlers/modelHandlerOpenAI.ts
@@ -579,7 +579,7 @@ export class ModelHandlerOpenAI extends ModelHandler {
 
     const lastMessage = messages.at(-1);
     if (hasEndTag(agentSetting, fileContent)) {
-      this.logger.info('End tag detected - skipping continuation');
+      this.logger.debug('End tag detected - skipping continuation');
       if (lastMessage && Array.isArray(lastMessage.content)) {
         // this is suspicious, because the two conflicts!!!
         const lastPart = lastMessage.content[lastMessage.content.length - 1];
