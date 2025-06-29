e09e82344ae14ab68fa8cddae313a00c758825dd
Sirui Lu <14889516+LionSR@users.noreply.github.com>
2025-06-29 16:18:20 +0200

    refactor: rename active stream state

--- src/progressView/modules/progressViewState.js
+++ src/progressView/modules/progressViewState.js
@@ -169,7 +169,7 @@ class StreamStatuses {
 export class ProgressViewState {
   constructor() {
     this.stateManager = new WebviewStateManager();
-    this.currentStream = '';
+    this.activeStream = '';
     this.currentGroupId = null;
 
     // Initialize managers
@@ -201,13 +201,13 @@ export class ProgressViewState {
     }
   }
 
-  // --- Current stream operations ---
-  getCurrentStream() {
-    return this.currentStream;
+  // --- Active stream operations ---
+  getActiveStream() {
+    return this.activeStream;
   }
 
-  setCurrentStream(stream) {
-    this.currentStream = stream;
+  setActiveStream(stream) {
+    this.activeStream = stream;
   }
 }
 
