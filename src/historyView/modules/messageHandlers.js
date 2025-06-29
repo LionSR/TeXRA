1a51f564bcd416ad3a268e022710cd514bb71257
Ray <divergence_lu@icloud.com>
2025-06-29 15:52:51 +0200

    feat: modularize message handlers

---
+++ src/historyView/modules/messageHandlers.js
@@ -0,0 +1,37 @@
+// Local imports
+import { registerMessageHandlers } from '@common/webviewContext.js';
+import { historyViewDomHandler } from './domHandlers.js';
+
+/**
+ * Handles messages from the extension for the history view.
+ */
+export class HistoryMessageHandlers {
+  constructor() {
+    this._cleanupFn = null;
+    this._handlers = {
+      updateHistory: (m) => this.handleUpdateHistory(m),
+      historyCleared: () => this.handleHistoryCleared(),
+    };
+  }
+
+  setup() {
+    this._cleanupFn = registerMessageHandlers(this._handlers);
+  }
+
+  cleanup() {
+    if (this._cleanupFn) {
+      this._cleanupFn();
+      this._cleanupFn = null;
+    }
+  }
+
+  handleUpdateHistory(message) {
+    historyViewDomHandler.renderer.render(message.historyItems);
+  }
+
+  handleHistoryCleared() {
+    historyViewDomHandler.renderer.render([]);
+  }
+}
+
+export const messageHandlers = new HistoryMessageHandlers();
