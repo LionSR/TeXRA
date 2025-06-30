5ffbbc9d931e0d525b6a878fe2d1f291f76d3550
Ray <divergence_lu@icloud.com>
2025-06-30 17:14:14 +0200

    feat: open files with default viewer

--- src/commands/files/openFileCommands.ts
+++ src/commands/files/openFileCommands.ts
@@ -1,13 +1,10 @@
 import * as vscode from 'vscode';
-import * as path from 'path';
-
 // Local imports - utilities
 import { openBuildDisplayIfTex } from '@frontend/latex/openBuild';
-import { WorkspaceFS } from '@utils/files';
+import { resolveFilePath } from '@utils/files';
 
 export async function openFile(file: string): Promise<void> {
-  const fullPath = path.isAbsolute(file) ? file : WorkspaceFS.fullPath(file);
-  const uri = vscode.Uri.file(fullPath);
+  const uri = vscode.Uri.file(resolveFilePath(file));
   await vscode.commands.executeCommand('vscode.open', uri);
 }
 
