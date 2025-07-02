27e6a6e874366d12621b85940a4bdcdc5a842b60
Sirui Lu <14889516+LionSR@users.noreply.github.com>
2025-07-02 21:14:59 +0200

    refactor: rename message handler classes

--- src/historyView/modules/messageHandlers.js
+++ src/historyView/modules/messageHandlers.js
@@ -5,7 +5,7 @@ import { historyViewDomHandler } from './domHandlers.js';
 /**
  * Handles messages from the extension for the history view.
  */
-export class HistoryMessageHandlers {
+export class HistoryViewMessageHandlers {
   constructor() {
     this._cleanupFn = null;
     this._handlers = {
@@ -34,4 +34,4 @@ export class HistoryMessageHandlers {
   }
 }
 
-export const messageHandlers = new HistoryMessageHandlers();
+export const messageHandlers = new HistoryViewMessageHandlers();
