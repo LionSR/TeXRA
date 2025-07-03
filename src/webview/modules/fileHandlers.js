51748a7ac33ab06ad5b710830acb655a8ac99efd
Sirui Lu <14889516+LionSR@users.noreply.github.com>
2025-07-03 21:44:42 +0200

    feat: map checkbox ids to update commands

--- src/webview/modules/fileHandlers.js
+++ src/webview/modules/fileHandlers.js
@@ -1,13 +1,18 @@
 import { vscode } from '@common/webviewContext.js';
-import { capitalize } from '@common/stringUtils.js';
+import { CHECKBOX_UPDATE_COMMANDS } from './constants.js';
 
 export function handleCheckboxChange(event) {
   const checkbox = event?.target || this;
   const checkboxId = checkbox.id;
   const isChecked = checkbox.checked;
 
+  const command = CHECKBOX_UPDATE_COMMANDS[checkboxId];
+  if (!command) {
+    return;
+  }
+
   vscode.postMessage({
-    command: `update${capitalize(checkboxId)}`,
+    command,
     value: isChecked,
   });
 }
