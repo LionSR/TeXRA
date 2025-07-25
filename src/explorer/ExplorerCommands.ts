a3a31ae3cec6b48fe5db880268d34e47534d296e
Sirui Lu <14889516+LionSR@users.noreply.github.com>
2025-07-25 12:28:13 +0200

    feat: add agent yaml to config from explorer

--- src/explorer/ExplorerCommands.ts
+++ src/explorer/ExplorerCommands.ts
@@ -33,6 +33,10 @@ export class ExplorerCommands {
         'texra.folderExplorer.openFile',
         (uri: vscode.Uri) => this.operations.open(uri),
       ),
+      vscode.commands.registerCommand(
+        'texra.folderExplorer.addToConfig',
+        (node: FileItem) => this.operations.addToConfig(node),
+      ),
     );
   }
 }
