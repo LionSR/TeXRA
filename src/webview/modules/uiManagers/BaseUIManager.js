fc9c95f6cb76b347db9ee067944fed162975f58d
Ray <divergence_lu@icloud.com>
2025-07-26 11:01:00 +0200

    feat: introduce BaseUIManager for webview

---
+++ src/webview/modules/uiManagers/BaseUIManager.js
@@ -0,0 +1,33 @@
+import { safeGetElementById } from '@common/domUtils.js';
+export class BaseUIManager {
+  constructor() {
+    this._listeners = [];
+  }
+
+  /**
+   * Register an event handler and store the listener for cleanup.
+   * @param {HTMLElement|string} elementOrId - element or its ID
+   * @param {string} event - event type
+   * @param {EventListenerOrEventListenerObject} handler - handler function
+   */
+  addListener(elementOrId, event, handler) {
+    const element =
+      typeof elementOrId === 'string'
+        ? safeGetElementById(elementOrId)
+        : elementOrId;
+    if (element) {
+      element.addEventListener(event, handler);
+      this._listeners.push({ element, event, handler });
+    }
+  }
+
+  /**
+   * Remove all registered event listeners.
+   */
+  cleanup() {
+    this._listeners.forEach(({ element, event, handler }) => {
+      element.removeEventListener(event, handler);
+    });
+    this._listeners = [];
+  }
+}
