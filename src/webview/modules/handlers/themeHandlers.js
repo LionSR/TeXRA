9ba72da3632fc3d6f879c580b4e7a85eda3d60f8
Ray <divergence_lu@icloud.com>
2025-09-10 19:57:48 +0200

    refactor: centralize webview theme handlers

--- src/webview/modules/handlers/themeHandlers.js
+++ src/webview/modules/handlers/themeHandlers.js
@@ -3,6 +3,7 @@ import { mainViewDomHandler } from '../domHandlers.js';
 // Local imports - DOM utilities
 import { safeSetElementValue } from '@common/domUtils.js';
 import { MAIN_VIEW_COMMANDS } from '@common/webview/commands.js';
+import { createThemeHandlers as createCommonThemeHandlers } from '@common/webview/themeHandlers.js';
 
 /**
  * Create handlers related to theme and model configuration.
@@ -10,19 +11,15 @@ import { MAIN_VIEW_COMMANDS } from '@common/webview/commands.js';
  * @param {Function} ctx.postHandle Function to call after handling a message.
  */
 export function createThemeHandlers({ postHandle }) {
+  const baseHandlers = createCommonThemeHandlers({
+    commands: MAIN_VIEW_COMMANDS,
+    postHandle,
+    onDebugModeChange: (debugMode) =>
+      mainViewDomHandler.setDebugMode(debugMode),
+  });
+
   return {
-    [MAIN_VIEW_COMMANDS.THEME_SET]: (message) => {
-      if (!message || typeof message.theme !== 'string') {
-        console.warn('Invalid theme message:', message);
-        return;
-      }
-      document.body.className = message.theme;
-      postHandle();
-    },
-    [MAIN_VIEW_COMMANDS.DEBUG_MODE_SET]: (message) => {
-      mainViewDomHandler.setDebugMode(message.debugMode);
-      postHandle();
-    },
+    ...baseHandlers,
     [MAIN_VIEW_COMMANDS.MODEL_SELECTED]: (message) => {
       safeSetElementValue('model', message.model);
       postHandle();
