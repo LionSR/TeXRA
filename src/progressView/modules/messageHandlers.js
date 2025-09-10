a96c89a98be403bb67779aead58148db0f9f54d0
Sirui Lu <14889516+LionSR@users.noreply.github.com>
2025-09-10 12:49:35 +0200

    refactor: centralize log appending helper

--- src/progressView/modules/messageHandlers.js
+++ src/progressView/modules/messageHandlers.js
@@ -2,6 +2,7 @@
 import { COMMANDS, STATUS, ELEMENT_IDS } from './constants.js';
 import { progressViewDomHandler, LogEntryFormatter } from './domHandlers.js';
 import { createThemeHandlers } from './handlers/themeHandlers.js';
+import { appendFormatted } from './utils.js';
 // Local imports - log state
 import { progressViewState } from './progressViewState.js';
 import { registerMessageHandlers } from '@common/webviewContext.js';
@@ -118,23 +119,11 @@ export class ProgressViewMessageHandler {
         if (msg.groupId) {
           if (!dom.logEntries.append(msg)) {
             const formatted = this._entryFormatter.format(msg);
-            if (formatted instanceof HTMLElement) {
-              logContent.appendChild(formatted);
-            } else {
-              const el = document.createElement('div');
-              el.innerHTML = formatted;
-              logContent.appendChild(el.firstElementChild);
-            }
+            appendFormatted(logContent, formatted);
           }
         } else {
           const formatted = this._entryFormatter.format(msg);
-          if (formatted instanceof HTMLElement) {
-            logContent.appendChild(formatted);
-          } else {
-            const el = document.createElement('div');
-            el.innerHTML = formatted;
-            logContent.appendChild(el.firstElementChild);
-          }
+          appendFormatted(logContent, formatted);
         }
       });
       logContent.scrollTop = logContent.scrollHeight;
@@ -176,12 +165,8 @@ export class ProgressViewMessageHandler {
               toggleIcon.className = 'codicon codicon-chevron-down toggle-icon';
             }
           }
-          logContent.appendChild(formatted);
-        } else {
-          const el = document.createElement('div');
-          el.innerHTML = formatted;
-          logContent.appendChild(el.firstElementChild);
         }
+        appendFormatted(logContent, formatted);
       }
       logContent.scrollTop = logContent.scrollHeight;
     }
@@ -189,7 +174,13 @@ export class ProgressViewMessageHandler {
 
   handleUpdateLog(message) {
     if (message.stream === state.activeStream) {
-      dom.logEntries.update(message.logMessage);
+      const logContent = document.getElementById(ELEMENT_IDS.LOG_CONTENT);
+      const updated = dom.logEntries.update(message.logMessage);
+      if (!updated) {
+        const formatted = this._entryFormatter.format(message.logMessage);
+        appendFormatted(logContent, formatted);
+        logContent.scrollTop = logContent.scrollHeight;
+      }
     }
   }
 
