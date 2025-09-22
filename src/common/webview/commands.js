b213eff614f680effa46328f6207ab56d1f3eb34
Ray <divergence_lu@icloud.com>
2025-09-22 17:45:07 +0200

    feat: surface active instruction in progress view

--- src/common/webview/commands.js
+++ src/common/webview/commands.js
@@ -159,6 +159,9 @@ export const PROGRESS_VIEW_COMMANDS = {
   APPEND_LOG: 'appendLog',
   UPDATE_LOG: 'updateLog',
 
+  // Instruction panel
+  UPDATE_INSTRUCTION: 'updateInstruction',
+
   // Task Groups
   ADD_TASK_GROUP: 'addTaskGroup',
   UPDATE_TASK_GROUP: 'updateTaskGroup',
