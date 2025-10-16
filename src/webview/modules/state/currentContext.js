0b90da60ecdd7f0823a05a34e9bacfcf6ddb9379
Ray <divergence_lu@icloud.com>
2025-10-16 11:09:53 +0200

    refactor: centralize main view context retrieval

---
+++ src/webview/modules/state/currentContext.js
@@ -0,0 +1,107 @@
+// Local imports - webview
+import {
+  MULTIPLE_SELECTIONS,
+  CHECK_BOXES,
+  ELEMENT_IDS,
+  SESSION_TYPES,
+  SESSION_TYPE_INPUT,
+  AGENT_SELECT_IDS,
+} from '../constants.js';
+import { fileList as defaultFileList } from '../uiManagers/FileList.js';
+import {
+  safeGetElementById,
+  safeGetElementValue,
+  safeGetElementChecked,
+} from '@common/domUtils.js';
+
+const DEFAULT_SINGLE_FILE_TYPES = ['input', 'reference', 'auxiliary', 'media'];
+
+function getSessionType() {
+  const sessionTypeElement = safeGetElementById(SESSION_TYPE_INPUT);
+  if (
+    sessionTypeElement instanceof HTMLInputElement &&
+    sessionTypeElement.value
+  ) {
+    return sessionTypeElement.value;
+  }
+  return SESSION_TYPES.WORKFLOW;
+}
+
+function getAgent(sessionType) {
+  const selectId =
+    AGENT_SELECT_IDS[sessionType] ?? AGENT_SELECT_IDS[SESSION_TYPES.WORKFLOW];
+  const selectElement = safeGetElementById(selectId);
+  if (selectElement instanceof HTMLSelectElement && selectElement.value) {
+    return selectElement.value;
+  }
+  return '';
+}
+
+function collectSingleFileSelections(singleFileTypes) {
+  const selections = {};
+  singleFileTypes.forEach((type) => {
+    const elementId = `${type}File`;
+    selections[elementId] = safeGetElementValue(elementId);
+  });
+  return selections;
+}
+
+function collectMultipleFileSelections(singleFiles, fileList) {
+  const selections = {};
+  MULTIPLE_SELECTIONS.forEach((id) => {
+    const container = safeGetElementById(`${id}Container`);
+    const isActive = container?.style.display !== 'none';
+    selections[`${id}Active`] = isActive;
+
+    const filesDiv = safeGetElementById(id);
+    const files = isActive && filesDiv ? fileList.getSelected(filesDiv) : [];
+
+    const singleFileKey = id.replace('Files', 'File');
+    const singleFileSelection = singleFiles[singleFileKey];
+    selections[id] =
+      id !== ELEMENT_IDS.OUTPUT_FILES && singleFileSelection
+        ? files.filter((file) => file !== singleFileSelection)
+        : files;
+  });
+  return selections;
+}
+
+function collectCheckboxValues() {
+  const values = {};
+  CHECK_BOXES.forEach((id) => {
+    values[id] = safeGetElementChecked(id);
+  });
+  return values;
+}
+
+/**
+ * Collect the current main view context from the DOM.
+ *
+ * @param {{
+ *   fileList?: import('../uiManagers/FileList.js').FileList,
+ *   singleFileTypes?: string[],
+ * }} [options]
+ */
+export function collectCurrentContext(options = {}) {
+  const {
+    fileList = defaultFileList,
+    singleFileTypes = DEFAULT_SINGLE_FILE_TYPES,
+  } = options;
+  const sessionType = getSessionType();
+  const agent = getAgent(sessionType);
+  const singleFileSelections = collectSingleFileSelections(singleFileTypes);
+  const multipleFileSelections = collectMultipleFileSelections(
+    singleFileSelections,
+    fileList,
+  );
+  const checkboxValues = collectCheckboxValues();
+
+  return {
+    agent,
+    sessionType,
+    isToolUseAgent: sessionType === SESSION_TYPES.TOOL_USE,
+    singleFileSelections,
+    multipleFileSelections,
+    checkboxValues,
+  };
+}
