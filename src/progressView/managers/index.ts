8b567714767303885ef6f8fc71d7523de7340c54
Ray <divergence_lu@icloud.com>
2025-11-07 22:29:26 +0100

    refactor: simplify progress state and view plumbing

--- src/progressView/managers/index.ts
+++ src/progressView/managers/index.ts
@@ -2,7 +2,5 @@ export { OutputFilesManager } from './OutputFilesManager';
 export { StreamTabsManager } from './StreamTabsManager';
 export { TaskGroupManager } from './TaskGroupManager';
 export type { TaskGroupUpdatePayload } from './TaskGroupManager';
-export { ToolUseTaskStateManager } from './ToolUseTaskStateManager';
 export { UsageStatsManager } from './UsageStatsManager';
 export { WebviewUpdater } from './WebviewUpdater';
-export { WorkflowTaskStateManager } from './WorkflowTaskStateManager';
