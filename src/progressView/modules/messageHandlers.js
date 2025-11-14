a3730a19f5b8aabb85648e53213116aeeb48b999
Ray <divergence_lu@icloud.com>
2025-11-14 22:11:55 +0100

    fix: reset recording state when stop is pressed

--- src/progressView/modules/messageHandlers.js
+++ src/progressView/modules/messageHandlers.js
@@ -147,6 +147,7 @@ export class ProgressViewMessageHandler extends BaseWebviewMessageHandler {
       [COMMANDS.FOLLOW_UP_TEXT_TRANSCRIBED]: (m) =>
         this.handleFollowUpTextTranscribed(m),
       [COMMANDS.RECORDING_STARTED]: () => this.handleRecordingStarted(),
+      [COMMANDS.RECORDING_STOPPED]: () => this.handleRecordingStopped(),
       [COMMANDS.RECORDING_ERROR]: () => this.handleRecordingError(),
     };
   }
@@ -688,6 +689,10 @@ export class ProgressViewMessageHandler extends BaseWebviewMessageHandler {
 
   handleRecordingStarted() {
     dom.followUpInput.setRecording(true);
+  }
+
+  handleRecordingStopped() {
+    dom.followUpInput.setRecording(false);
   }
 
   handleRecordingError() {
