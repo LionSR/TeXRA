b3af2672a4fbb191cc11fc1084eb4ba7ac2ddc0b
Sirui Lu <14889516+LionSR@users.noreply.github.com>
2026-05-02 23:19:45 +0200

    fix: use camelcase binary resolver module

--- src/utils/system/index.ts
+++ src/utils/system/index.ts
@@ -2,6 +2,6 @@ export * from './execUtils';
 export * from './gitAuthorEnv';
 export * from './toolUtils';
 export * from './platformPaths';
-export * from './BinaryResolver';
+export * from './binaryResolver';
 export * from './wslDetect';
 export * from './workspaceInfo';
