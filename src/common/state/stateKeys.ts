96ff227058ace558247a44352cb779273cbc0240
Ray <divergence_lu@icloud.com>
2026-05-02 15:33:09 +0200

    feat: configure Codex approval policy (#3263)

--- src/common/state/stateKeys.ts
+++ src/common/state/stateKeys.ts
@@ -42,6 +42,7 @@ export enum WorkspaceStateKey {
   // Codex settings
   CODEX_SANDBOX_MODE = 'texra.codexSandboxMode',
   CODEX_REASONING_EFFORT = 'texra.codexReasoningEffort',
+  CODEX_APPROVAL_POLICY = 'texra.codexApprovalPolicy',
 
   // Git commit author settings
   GIT_MARK_COMMITS = 'texra.git.markCommits',
