138966249cca2252ef1f402c5b2181fa8fd1bc8f
Sirui Lu <14889516+LionSR@users.noreply.github.com>
2026-05-03 06:37:35 +0200

    refactor: introduce UI host ports

---
+++ src/frontend/hosts/index.ts
@@ -0,0 +1,5 @@
+export { VscodeClipboardHost } from './VscodeClipboardHost';
+export { VscodeExternalOpener } from './VscodeExternalOpener';
+export { VscodePromptHost } from './VscodePromptHost';
+export { VscodeTerminalHost } from './VscodeTerminalHost';
+export { createVscodeUIHosts } from './vscodeUIHosts';
