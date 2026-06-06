cdc3853b8a5c66b4f85be13a5583c384db77c04c
Sirui Lu <divergence_lu@icloud.com>
2026-06-06 19:43:02 +0200

    refactor: share progress event handler (#5459)

--- packages/extension/src/progressView/events/index.ts
+++
@@ -1,4 +0,0 @@
-// Barrel export for progress view events
-export { ProgressEventHandler, type UICallbacks } from './ProgressEventHandler';
-export { registerUIEvents } from './UIEvents';
-export { type EventHandlerContext } from './EventHandlerContext';
