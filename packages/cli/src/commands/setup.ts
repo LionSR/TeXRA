317be88deb90bbcff39fe2f9c10a6a6ec28a2fc3
Sirui Lu <14889516+LionSR@users.noreply.github.com>
2026-06-10 23:21:03 +0200

    refactor: remove internal CLI dispatch barrel

--- packages/cli/src/commands/setup.ts
+++ packages/cli/src/commands/setup.ts
@@ -9,7 +9,7 @@ import {
 } from '../runtime/terminalRequirements';
 
 import { contextFromArgs } from './_helpers/context';
-import { withUsageSections } from './_helpers/dispatch';
+import { withUsageSections } from './_helpers/dispatch/usage';
 import { setExitCode } from './_helpers/exitCode';
 import {
   INTERACTIVE_GLOBAL_ARGS,
