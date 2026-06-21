ecbade7d190c575200991bbb60ad6bb6750bc0d6
Sirui Lu <14889516+LionSR@users.noreply.github.com>
2026-06-21 12:19:24 +0200

    refactor: move file selection registry to frontend (#6398)

--- packages/extension/src/commands/files/fileSelectionCommands.ts
+++ packages/extension/src/commands/files/fileSelectionCommands.ts
@@ -3,8 +3,8 @@ import * as vscode from 'vscode';
 
 // Local imports - log
 import { registerCommands } from '@commands/_shared/registerCommands';
-import { FILE_SELECTION_COMMAND_IDS, getFilterExtensions } from '@common/files';
-import { getFileLister } from '@frontend/files';
+import { getFilterExtensions } from '@common/files';
+import { FILE_SELECTION_COMMAND_IDS, getFileLister } from '@frontend/files';
 import { showLoggedErrorMessage } from '@frontend/ui/errorHandlingUtils';
 import { selectFile, selectFiles } from '@frontend/ui/dialogs';
 import * as logger from '@logger/logUtils';
--- packages/extension/src/frontend/files/index.ts
+++ packages/extension/src/frontend/files/index.ts
@@ -2,3 +2,10 @@
 export { FileLister, getFileLister } from './fileLister';
 export type { ListableFileType } from '@common/files/fileListingRules';
 export { getFilesRecursively } from './listing';
+export {
+  FILE_SELECTION_COMMAND_IDS,
+  MULTIPLE_FILE_COMMANDS,
+  type FileSelectionCommandId,
+  type FileSelectionResponseCommand,
+  type MultiFileCategory,
+} from './fileSelectionRegistry';
