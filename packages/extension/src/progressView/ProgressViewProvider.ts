9828a0413d6fede03a8465cf9e806befc52b71c2
Sirui Lu <14889516+LionSR@users.noreply.github.com>
2026-07-07 00:10:27 +0200

    refactor: route goal state consumers through sessions (#7339)

--- packages/extension/src/progressView/ProgressViewProvider.ts
+++ packages/extension/src/progressView/ProgressViewProvider.ts
@@ -113,11 +113,6 @@ export class ProgressViewProvider
       getStreamControls: getProgressStreamControls,
       getUnsupportedCommands: () =>
         this.messageHandler.getUnsupportedCommands(),
-      onSessionProgressEvent: (event, payload) => {
-        if (event === 'goalStateChanged') {
-          bus.emit(event, payload);
-        }
-      },
       configureUi: ({ webviewUpdater: u }) => {
         const canSend = () => this.canSendToWebview();
         this.approvalHandlers = buildApprovalRequestHandlerSet({
