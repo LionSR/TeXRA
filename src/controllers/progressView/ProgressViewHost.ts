bbf1672d88b898702a0cf5a549416d5d60df0c69
Sirui Lu <14889516+LionSR@users.noreply.github.com>
2026-07-08 02:39:42 +0200

    refactor(progress): share progress view host wiring

---
+++ src/controllers/progressView/ProgressViewHost.ts
@@ -0,0 +1,129 @@
+// Local imports - controllers
+import {
+  createProgressViewCommandHandlers,
+  type ProgressViewApprovalCommandActions,
+  type ProgressViewBypassCommandOptions,
+  type ProgressViewExternalInquiryCommandActions,
+  type ProgressViewFileCommandActions,
+  type ProgressViewFollowUpCommandActions,
+  type ProgressViewLifecycleCommandActions,
+  type ProgressViewRunCommandActions,
+} from './ProgressViewCommandHandlers';
+import {
+  ProgressAgentProposalController,
+  type ProgressAgentProposalControllerDeps,
+} from './ProgressAgentProposalController';
+import {
+  ProgressWorkflowActionsController,
+  type ProgressWorkflowActionsControllerDeps,
+} from './ProgressWorkflowActionsController';
+import {
+  ProgressWorkflowFileActionsController,
+  type ProgressWorkflowFileActionsControllerDeps,
+} from './ProgressWorkflowFileActionsController';
+
+type ProgressViewFileHostActions = Pick<
+  ProgressViewFileCommandActions,
+  'openFile' | 'openFileCompile'
+> &
+  Partial<Omit<ProgressViewFileCommandActions, 'openFile' | 'openFileCompile'>>;
+
+type ProgressViewApprovalHostActions = Omit<
+  ProgressViewApprovalCommandActions,
+  'handleAgentProposalAction'
+> &
+  Partial<
+    Pick<ProgressViewApprovalCommandActions, 'handleAgentProposalAction'>
+  >;
+
+export interface ProgressViewHostCommandOptions {
+  readonly lifecycle: ProgressViewLifecycleCommandActions;
+  readonly run?: Partial<ProgressViewRunCommandActions>;
+  readonly followUp: ProgressViewFollowUpCommandActions;
+  readonly bypass: ProgressViewBypassCommandOptions;
+  readonly file: ProgressViewFileHostActions;
+  readonly approval: ProgressViewApprovalHostActions;
+  readonly externalInquiry: ProgressViewExternalInquiryCommandActions;
+}
+
+export interface ProgressViewHostOptions {
+  readonly workflowActions: ProgressWorkflowActionsControllerDeps;
+  readonly workflowFileActions: ProgressWorkflowFileActionsControllerDeps;
+  readonly agentProposal: ProgressAgentProposalControllerDeps;
+  readonly commands: ProgressViewHostCommandOptions;
+}
+
+export class ProgressViewHost {
+  readonly workflowActionsController: ProgressWorkflowActionsController;
+  readonly workflowFileActionsController: ProgressWorkflowFileActionsController;
+  readonly agentProposalController: ProgressAgentProposalController;
+  readonly commandHandlers: ReturnType<
+    typeof createProgressViewCommandHandlers
+  >;
+
+  constructor(options: ProgressViewHostOptions) {
+    this.workflowActionsController = new ProgressWorkflowActionsController(
+      options.workflowActions,
+    );
+    this.workflowFileActionsController =
+      new ProgressWorkflowFileActionsController(options.workflowFileActions);
+    this.agentProposalController = new ProgressAgentProposalController(
+      options.agentProposal,
+    );
+    this.commandHandlers = createProgressViewCommandHandlers({
+      lifecycle: options.commands.lifecycle,
+      run: {
+        resumeStream:
+          options.commands.run?.resumeStream ??
+          ((stream) => this.workflowActionsController.resume(stream)),
+        runNewStream:
+          options.commands.run?.runNewStream ??
+          ((stream) => this.workflowActionsController.runNew(stream)),
+      },
+      followUp: options.commands.followUp,
+      bypass: options.commands.bypass,
+      file: {
+        openFile: options.commands.file.openFile,
+        openFileCompile: options.commands.file.openFileCompile,
+        openTaskStorage:
+          options.commands.file.openTaskStorage ??
+          ((stream) =>
+            this.workflowFileActionsController.openTaskStorage(stream)),
+        compareOriginal:
+          options.commands.file.compareOriginal ??
+          ((file, base) =>
+            this.workflowFileActionsController.compareOriginal(file, base)),
+        comparePrevious:
+          options.commands.file.comparePrevious ??
+          ((file, base, previous) =>
+            this.workflowFileActionsController.comparePrevious(
+              file,
+              base,
+              previous,
+            )),
+        acceptFile:
+          options.commands.file.acceptFile ??
+          ((file, base) =>
+            this.workflowFileActionsController.acceptFile(file, base)),
+        mergeFile:
+          options.commands.file.mergeFile ??
+          ((file, base) =>
+            this.workflowFileActionsController.mergeFile(file, base)),
+        latexdiffFile:
+          options.commands.file.latexdiffFile ??
+          ((file, base) =>
+            this.workflowFileActionsController.latexdiffFile(file, base)),
+        openLabel:
+          options.commands.file.openLabel ??
+          ((label) => this.workflowFileActionsController.openLabel(label)),
+      },
+      approval: {
+        ...options.commands.approval,
+        handleAgentProposalAction:
+          options.commands.approval.handleAgentProposalAction ??
+          ((message) => this.agentProposalController.handleAction(message)),
+      },
+      externalInquiry: options.commands.externalInquiry,
+    });
+  }
+}
