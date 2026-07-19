a0f6fb6717d8a5b9d0c9102f6ebf6144eb449003
Sirui Lu <divergence_lu@icloud.com>
2026-07-19 13:29:02 +0200

    refactor: unify authentication logging (#8912)
    * refactor: unify authentication logging
    
    * docs: correct authentication logger comment

--- src/auth/serviceLogger.ts
+++
@@ -1,9 +0,0 @@
-export interface AuthServiceLogger {
-  info(channel: string, message: string): void;
-  error(channel: string, message: string): void;
-}
-
-export const NOOP_AUTH_SERVICE_LOGGER: AuthServiceLogger = {
-  info() {},
-  error() {},
-};
